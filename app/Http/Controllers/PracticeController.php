fb3171cd201de7c576bec95b1fb9dbd3b53583b1
comi190327 <comi@actails.com>
2022-08-15 22:12:41 +0900

    PHP03 HTML表示に対応

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -5,16 +5,19 @@ class PracticeController extends Controller
 {
     public function sample()
     {
-        return response('practice');
+        // return response('practice');
+        return view('practice');
     }
     public function sample2()
     {
         $test = 'practice2';
-        return response($test);
+        // return response($test);
+        return view('practice2', ['testParam' => $test]);
     }
     public function sample3()
     {
         $value = 'test';
-        return response($value);
+        // return response($value);
+        return view('practice3', ['testParam' => $value]);
     }
 }
